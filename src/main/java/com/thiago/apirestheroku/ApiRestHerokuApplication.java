2c6cffb775df76be39c776d000fef4a140f470cc
Thiago Ferreira <tlrferreira83@gmail.com>
2022-08-13 05:58:02 -0300

    Project created

---
+++ src/main/java/com/thiago/apirestheroku/ApiRestHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.thiago.apirestheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestHerokuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiago/apirestheroku/ApiRestHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.apirestheroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestHerokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
